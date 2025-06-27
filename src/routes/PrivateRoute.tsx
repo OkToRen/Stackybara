c099bb5613c08d56b754d7cba8d66830be883400
OkToRen <darrenharyanto@gmail.com>
2025-06-27 18:11:28 +0700

    Fix: PrivateRoute Returns layout not Outlet

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,10 +1,11 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuthContext } from '@/lib/AuthContext';
+import Layout from '@/layout/Layout';
 
 export const PrivateRoute = () => {
   const auth = useAuthContext();
 
   if (auth.loginLoading) return <div>Loading...</div>; // Or a spinner component
 
-  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+  return auth.isAuthenticated ? <Layout /> : <Navigate to="/" replace />;
 };
