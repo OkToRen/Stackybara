b547f97f69812122a75f322386f4f64a1b5b1356
MisMoela <martinerickson2008@gmail.com>
2025-06-29 21:31:22 +0700

    Fix : Added Wallet Button and Chabot Button to SellerLayout

--- src/layout/SellerLayout.tsx
+++ src/layout/SellerLayout.tsx
@@ -1,16 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import SellerHeader from '@/components/SellerHeader';
 import Footer from '@/components/Footer'; 
-
-
+import FloatingChatButton from "@/components/ui/floatingchatbutton"
+import FloatingWalletButton from "@/components/ui/floatingwalletbutton"
 
 export default function SellerLayout() {
+  const location = useLocation()
+  const isChatPage = location.pathname.startsWith("/chat")
+  const isWalletPage = location.pathname.startsWith("/wallet")
+
   return (
     <>
       <SellerHeader />
       <main>
         <Outlet />
       </main>
+      {!isChatPage && <FloatingChatButton />}
+      {!isWalletPage && <FloatingWalletButton />}
       <Footer /> 
     </>
   );
